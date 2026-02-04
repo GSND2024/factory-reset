308e6e9e50e8dd5d145444314226721b59bf580a
Stephen Singler <stephen.singler@gmail.com>
2026-02-03 20:38:53 -0800

    quick continuity edits for playtesting
    repurposed old prototypelevel5 for ending, and had to direct door from level 4 to new level 5

--- Assets/Scripts/Level6Trigger.cs
+++ Assets/Scripts/Level6Trigger.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement; 
 
-public class Level5Trigger : MonoBehaviour
+public class Level6Trigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             GlobalGameState.isLevel4 = false;
-            SceneManager.LoadScene("PrototypeLevel6");
+            SceneManager.LoadScene("PrototypeLevel5");
         }
     }
 }
