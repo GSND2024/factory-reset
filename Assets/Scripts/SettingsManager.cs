d645182c2a7a5dd1354b7e66cb80c6e383fb6e49
Stephen Singler <stephen.singler@gmail.com>
2026-04-05 22:43:46 -0700

    quick changes

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -184,7 +184,7 @@ public class SettingsManager : MonoBehaviour
         if (!isEnabledInCurrentScene)
             return;
             
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && !PanelToggleUI.IsPanelOpen)
         {
             if (isSettingsOpen)
             {
